6c7419553aa00c0b49ec0f09548b425e564b073c
carlospardoo <28719833+carlospardoo@users.noreply.github.com>
2023-09-27 21:42:53 -0500

    modified sql scripts

--- persistence/Script_Creacion.sql
+++ persistence/Script_Creacion.sql
@@ -1,5 +1,10 @@
 
---create database Cadena_Hoteles
+--create database Cadena_Hoteles 
+--CONTAINMENT = partial 
+--collate SQL_Latin1_General_CP1_CI_AS 
+--with 
+--	default_language = 3082,
+--	DEFAULT_FULLTEXT_LANGUAGE = 3082
 --go
 
 use Cadena_Hoteles
@@ -79,5 +84,22 @@ values
 
 insert into Dim_Temporada(Nombre_Temporada, Costo_Hora, Fecha_inicio, Fecha_Fin)
 values
-('ALTA', 15000, '12/01/2023', '01/02/2024'),
-('BAJA', 15000, '02/01/2023', '02/28/2023')
+('ALTA', 15000, '01/12/2023', '08/01/2024'),
+('BAJA', 50, '01/02/2023', '28/02/2023')
+
+insert into Dim_Persona(Nombre, Apellido, Telefono, Email)
+values
+('Carlos', 'Pardo', '+57 3213933592', 'carlos.pardo@mail.com'),
+('Juana', 'Tello', '+52 4567891230', 'juana.tello@mail.com')
+
+insert into Fact_Habitacion(Piso, Numero_Habitacion, Capacidad, Id_Sede, Id_Tipo_Habitacion)
+values
+('7', 701, 4, 4, 1),
+('1', 101, 3, 4, 3),
+('2', 201, 1, 4, 2),
+('1', 1, 4, 1, 1)
+
+insert into Fact_Ocupacion_Habitacion( Fecha_Entrada, Fecha_Salida, Costo_Total, Id_Temporada, Id_Habitacion, Id_Persona )
+values
+('01/12/2023 18:00:00', '08/01/2024 8:00:00', (DATEDIFF(hour, '01/12/2023 18:00:00', '01/08/2024 8:00:00') * 50) + (DATEDIFF(day, '01/12/2023 18:00:00', '08/01/2024 8:00:00') * 40000) , 1, 1, 1 ),
+('01/10/2023 12:00:00', '15/10/2023 12:00:00', (DATEDIFF(hour, '01/10/2023 12:00:00', '15/10/2023 12:00:00') * 50) + (DATEDIFF(day, '01/10/2023 12:00:00', '15/10/2023 12:00:00') * 40000) , 1, 2, 2 )
--- persistence/sp_Get_Disponibilidad.sql
+++ persistence/sp_Get_Disponibilidad.sql
@@ -8,13 +8,14 @@ GO
 -- Create date: 15/09/2023
 -- Description:	sp para definir consultas de disponibilidad
 -- =============================================
-CREATE PROCEDURE sp_Get_Disponibilidad 
+-- sp_Get_Disponibilidad '2023/12/01 19:00:00', 4
+CREATE OR ALTER PROCEDURE sp_Get_Disponibilidad 
 	
-	@Fecha_Viaje datetime not null,
-	@Id_Sede int,
-	@Id_Temporada int,
-	@Id_Tipo_Habitacion int,
-	@Numero_Personas int
+	@Fecha_Viaje datetime,
+	@Id_Sede int = null,
+	@Id_Temporada int = null,
+	@Id_Tipo_Habitacion int = null,
+	@Numero_Personas int = null
 
 AS
 BEGIN
@@ -22,16 +23,20 @@ BEGIN
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
 
-    -- Insert statements for procedure here
+    -- PENDIENTE CALCULO DE LA TEMPORADA
 	SELECT 
 		a.Id_Habitacion,
 		a.Piso,
 		a.Numero_Habitacion,
 		a.Capacidad
 	FROM Fact_Habitacion a 
+	LEFT JOIN Fact_Ocupacion_Habitacion d on a.Id_Habitacion = d.Id_Habitacion
 	INNER JOIN Dim_Tipo_Habitacion b on a.Id_Tipo_Habitacion = b.Id_Tipo_Habitacion
 	INNER JOIN Dim_Sede c on a.Id_Sede = c.Id_Sede
-	
+	WHERE @Fecha_Viaje not between d.Fecha_Entrada and d.Fecha_Salida or d.Fecha_Entrada is null
+		and (@Id_Sede = c.Id_Sede or @Id_Sede is null)
+		and (@Id_Tipo_Habitacion = a.Id_Tipo_Habitacion or @Id_Tipo_Habitacion is null)
+		and (@Numero_Personas <= a.Capacidad or @Numero_Personas is null)
 
 
 END
